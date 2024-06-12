3702d667ceeb648bc09cd89b5034b565fea59881
Edmar Gabriel <edmar_gabriel@sicredi.com.br>
2024-06-12 08:08:15 -0300

    enviando para o github

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    import { base } from '$app/paths';
+    export const prerender = true;
+</script>
+
+<nav>
+    <a href="{base}/">Home</a>
+    <a href="{base}/about">About</a>
+</nav>
